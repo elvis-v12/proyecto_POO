51953319a01087044094fd53eb1ea4b20b41dc87
GatomontesRoseIII <elnoe21827@gmail.com>
2023-10-04 20:18:51 -0500

    Cambios de Noe con el orden de carpetas y renombamiento y el formulario jfrmAlumno

---
+++ src/SoftwareMatricula/controller/AlumnoController.java
@@ -0,0 +1,9 @@
+package SoftwareMatricula.controller;
+
+/**
+ *
+ * @author Noe Elvis Ramirez Herrera
+ */
+public class AlumnoController {
+
+}
---
+++ src/SoftwareMatricula/model/Administrador.java
@@ -0,0 +1,12 @@
+package SoftwareMatricula.model;
+
+/**
+ *
+ * @author Noe Elvis Ramirez Herrera
+ */
+public class Administrador {
+
+//atributos
+//constructor
+//metodos
+}
