91df558d34ed2f6b56aed61d0e882812e3bd707b
BlueGit <blue@git.com>
2023-10-26 18:59:49 -0500

    Sbubiendo carpetas nuevas

---
+++ src/SoftwareLaboratorio/SoftwareMatriculaAppliation.java
@@ -0,0 +1,18 @@
+package SoftwareLaboratorio;
+
+
+
+import SoftwareLB.view.registro.jfrmRegistroAlumno;
+
+/**
+ *
+ * @author Noe Elvis Ramirez Herrera
+ *
+ */
+public class SoftwareMatriculaAppliation {
+
+        public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(() -> new jfrmRegistroAlumno().setVisible(true));
+        }
+
+}
